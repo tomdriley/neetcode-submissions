78086c44be723761003ed3de11f17ef176c66216
Tom Riley <mail@thomasriley.ca>
2026-05-19 22:23:30 -0400

    Add: subtree-of-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-6.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def tree_hash(root: Optional[TreeNode]) -> str:
+    if root is None:
+        return "#"
+    return f",{root.val},{tree_hash(root.left)},{tree_hash(root.right)}"
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        return tree_hash(subRoot) in tree_hash(root)
